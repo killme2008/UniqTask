027e31277379a031b311b74b6b4320ee536104ee
dennis zhuang <killme2008@gmail.com>
2011-10-06 23:03:01 +0800

    Add chinese values

--- gen/com/github/killme2008/utask/R.java
+++ gen/com/github/killme2008/utask/R.java
@@ -29,20 +29,20 @@ public final class R {
     }
     public static final class layout {
         public static final int about=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int process=0x7f030002;
-        public static final int process_list=0x7f030003;
+        public static final int process=0x7f030001;
+        public static final int process_list=0x7f030002;
     }
     public static final class string {
-        public static final int about_app=0x7f050007;
-        public static final int about_desc=0x7f050008;
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
-        public static final int kill_label=0x7f050005;
-        public static final int no_processes=0x7f050002;
-        public static final int progress_message=0x7f050004;
-        public static final int progress_title=0x7f050003;
-        public static final int refresh_label=0x7f050006;
+        public static final int about_app=0x7f050008;
+        public static final int about_desc=0x7f050009;
+        public static final int app_name=0x7f050000;
+        public static final int available_mem=0x7f050003;
+        public static final int kill_label=0x7f050006;
+        public static final int no_processes=0x7f050001;
+        public static final int process=0x7f050002;
+        public static final int progress_message=0x7f050005;
+        public static final int progress_title=0x7f050004;
+        public static final int refresh_label=0x7f050007;
     }
     public static final class style {
         public static final int info=0x7f060001;
